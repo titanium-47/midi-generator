dd754319e0e6d7b5603537bfb10b2c668c3dd37a
Sidharth R <rsidharth53@gmail.com>
2023-05-31 16:37:55 -0700

    fft works to some extent

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import numpy as np
 from midiutil.MidiFile import MIDIFile
 import matplotlib.pyplot as plt
 from scipy import fft
-from scipy.stats import mode
+import math
+
+CHUNK_SIZE = 4096
 
 audio = wave.open(f"{os.getcwd()}\\audio\\Test_2.wav", 'rb')
 frame_rate = audio.getframerate()
@@ -15,13 +17,41 @@ duration = num_frames/frame_rate
 frames = audio.readframes(num_frames)
 audio.close()
 
-times = np.linspace(0, duration, num=num_frames)
-
 audio_signal = np.frombuffer(frames, dtype=np.int16)
-plt.figure(figsize=(15, 5))
-plt.plot(times, audio_signal)
-plt.title('Left Channel')
-plt.ylabel('Signal Value')
-plt.xlabel('Time (s)')
-plt.xlim(0, duration)
+
+x = CHUNK_SIZE
+frequencies = []
+while x<num_frames:
+    yfft = fft(audio_signal[x-CHUNK_SIZE:x])[:int(CHUNK_SIZE*0.75)]
+    frequency = np.argmax(yfft)*frame_rate/CHUNK_SIZE
+    if np.amax(yfft) > 200000:
+        frequencies.append(frequency)
+    else:
+        frequencies.append(0)
+    x+=CHUNK_SIZE
+print(frequencies)
+print(np.argmax(yfft))
+plt.plot(frequencies)
+
+mf = MIDIFile(1)     # only 1 track
+track = 0   # the only track
+
+time = 0    # start at the beginning
+mf.addTrackName(track, time, "Sample Track")
+mf.addTempo(track, time, 6000)
+
+# add some notes
+channel = 0
+note_length = duration/len(frequencies)
+for value in frequencies:
+    if(value != 0):
+        volume = 100
+        pitch = 12*math.log2(value/220.0)+57
+        mf.addNote(track, channel, int(pitch), int(time*100.0), int(note_length*100.0), int(volume))
+    time += note_length
+
+# write it to disk
+with open("output.mid", 'wb') as outf:
+    mf.writeFile(outf)
+
 plt.show()
