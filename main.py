4a285172db29ad9ceccb0b8dd8158698e758bc5c
Sidharth R <rsidharth53@gmail.com>
2023-05-30 21:51:15 -0700

    audio visualizer

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import wave
 import os
 import numpy as np
+from midiutil.MidiFile import MIDIFile
+import matplotlib.pyplot as plt
+from scipy import fft
+from scipy.stats import mode
 
 audio = wave.open(f"{os.getcwd()}\\audio\\Test_2.wav", 'rb')
-sample_frequency = audio.getframerate()
-sample_number = audio.getnframes()
-sample_duration = sample_number/sample_frequency
-sample_channels = audio.getnchannels()
+frame_rate = audio.getframerate()
+num_frames = audio.getnframes()
+duration = num_frames/frame_rate
 
-print(f"{sample_channels}\n{sample_duration}\n{sample_number}\n{sample_frequency}")
+# Read all frames
+frames = audio.readframes(num_frames)
+audio.close()
 
+times = np.linspace(0, duration, num=num_frames)
+
+audio_signal = np.frombuffer(frames, dtype=np.int16)
+plt.figure(figsize=(15, 5))
+plt.plot(times, audio_signal)
+plt.title('Left Channel')
+plt.ylabel('Signal Value')
+plt.xlabel('Time (s)')
+plt.xlim(0, duration)
+plt.show()
