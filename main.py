81ce922106848a051cf98f11b85a860e829b3759
Sidharth R <rsidharth53@gmail.com>
2023-05-31 22:57:56 -0700

    added support for multiple tracks

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from scipy import fft
 import math
 
 CHUNK_SIZE = 1024
+TRACK_AMOUNT = 8
 
 fig, (ax1, ax2) = plt.subplots(2, figsize=(15, 7))
 
-audio = wave.open(f"{os.getcwd()}\\audio\\Test.wav", 'rb')
+audio = wave.open(f"{os.getcwd()}\\audio\\Test_3.wav", 'rb')
 frame_rate = audio.getframerate()
 num_frames = audio.getnframes()
 duration = num_frames/frame_rate
@@ -26,36 +27,42 @@ sound = []
 
 while x<num_frames:
     yfft = fft(audio_signal[x-CHUNK_SIZE:x])[:int(CHUNK_SIZE*0.75)]
-    frequency = np.argmax(yfft)*frame_rate/CHUNK_SIZE
-    volume = (abs(np.amax(yfft)))
-    sound.append([frequency,volume])
+    frequencies = np.argsort(yfft)[len(yfft)-TRACK_AMOUNT:]
+    volumes = np.sort(yfft)[len(yfft)-TRACK_AMOUNT:]
+    sound.append([frequencies, volumes])
     x+=CHUNK_SIZE
 
 sound = np.array(sound)
+max_volumes = []
 
-max_volume = np.amax(sound[:,1])
+for i in range(0, TRACK_AMOUNT):
+    max_volumes.append(np.amax(sound[:,1,i]))
 
-for i in range(0,len(sound)):
-    sound[i][1] = sound[i][1]/max_volume*100
+max_volume = max(max_volumes)
 
-ax1.plot(sound[:,0])
-ax2.plot(sound[:,1])
+for i in range(0, len(sound)):
+    for j in range(0, TRACK_AMOUNT):
+        sound[i][1][j] = sound[i][1][j]/max_volume*100
 
-mf = MIDIFile(1)     # only 1 track
-track = 0   # the only track
+ax1.plot(sound[:,0,0])
+ax2.plot(sound[:,1,0])
 
-time = 0    # start at the beginning
-mf.addTrackName(track, time, "Sample Track")
-mf.addTempo(track, time, 6000)
+mf = MIDIFile(TRACK_AMOUNT)     # only 1 track
+time = 0    
+
+for i in range(0, TRACK_AMOUNT):
+    mf.addTrackName(i, time, f"Track {i}")
+    mf.addTempo(i, time, 6000)
 
 # add some notes
 channel = 0
 note_length = duration/len(sound)
-for value in sound:
-    if(value[0] != 0):
-        volume = abs(value[1])
-        pitch = 12*math.log2(value[0]/220.0)+57
-        mf.addNote(track, channel, int(pitch), int(time*100.0), int(note_length*100.0), int(volume))
+for value in sound[1:]:
+    for i in range(0, TRACK_AMOUNT):
+        if(value[0][i] != 0):
+            volume = abs(value[1][i])
+            pitch = 12*math.log2(value[0][i]*frame_rate/CHUNK_SIZE/220.0)+57
+            mf.addNote(i, channel, int(pitch), int(time*100.0), int(note_length*100.0), int(volume))
     time += note_length
 
 # write it to disk
